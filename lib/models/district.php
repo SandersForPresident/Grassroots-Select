9b5c387cb43005517fd8eb67c21e12e7fac490b5
Atticus White <contact@atticuswhite.com>
2015-08-24 00:42:27 -0400

    template integration

--- lib/models/district.php
+++ lib/models/district.php
@@ -7,13 +7,14 @@ class DistrictModel extends AbstractModel {
   public $pvi;
   public $party;
   public $state;
+  public $candidates;
 
   public function __construct($post) {
     parent::__construct($post);
 
     $this->number = get_field('district_number', $this->post->ID);
     $this->pvi = get_field('pvi', $this->post->ID);
-    $this->candidates = get_field('candidate', $this->post->ID);
+    $this->candidates = get_field('candidates', $this->post->ID);
 
     $partyTerms = wp_get_post_terms($this->post->ID, 'party');
     $this->party = $partyTerms[0];
--- taxonomy-state.php
+++ taxonomy-state.php
@@ -1,18 +1,38 @@
 <?php
   get_header();
   use GrassrootsSelect\Models\DistrictModel;
-  global $post;
 
+  global $post;
+  $state = $wp_query->queried_object;
   $districts = array();
+
   while (have_posts()) {
     the_post();
     $districts[] = new DistrictModel($post);
   }
-
 ?>
+<div>
+  <h1>State: <?php echo $state->name; ?></h1>
+</div>
+
+
 <?php foreach($districts as $district): ?>
 <div>
   <h2><a href="<?php echo $district->getPermalink(); ?>"><?php echo $district->getTitle(); ?></a></h2>
+  <h3>Party: <?php echo $district->party->name; ?></h3>
+  <h3>PVI: <?php echo $district->pvi; ?></h3>
+  <h3>Candidates:</h3>
+  <ul>
+    <?php foreach($district->candidates as $candidate): ?>
+      <li>
+        <pre>
+          <?php print_r($candidate); ?>
+        </pre>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+
+
 </div>
 <?php endforeach; ?>
 
