cdc2219f9be30940669bd0b1e67279cdc36b104e
Atticus White <contact@atticuswhite.com>
2015-08-24 00:33:47 -0400

    use district model

--- taxonomy-state.php
+++ taxonomy-state.php
@@ -1,7 +1,19 @@
-<?php get_header(); ?>
-<?php while(have_posts()): the_post(); ?>
-  <div>
-    <h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
-  </div>
-<?php endwhile; ?>
+<?php
+  get_header();
+  use GrassrootsSelect\Models\DistrictModel;
+  global $post;
+
+  $districts = array();
+  while (have_posts()) {
+    the_post();
+    $districts[] = new DistrictModel($post);
+  }
+
+?>
+<?php foreach($districts as $district): ?>
+<div>
+  <h2><a href="<?php echo $district->getPermalink(); ?>"><?php echo $district->getTitle(); ?></a></h2>
+</div>
+<?php endforeach; ?>
+
 <?php get_footer(); ?>
