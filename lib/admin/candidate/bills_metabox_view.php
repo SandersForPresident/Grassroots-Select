4af364cb9f1e32224a3818142b9cd502d8105861
Atticus White <contact@atticuswhite.com>
2015-09-12 20:03:17 -0400

    persist candidate voting history

--- lib/admin/candidate/bills_metabox_view.php
+++ lib/admin/candidate/bills_metabox_view.php
@@ -12,15 +12,22 @@
       <td><?php echo $bill->getTitle(); ?></td>
       <td><?php echo $bill->getIssueTitle(); ?></td>
       <td>
-        <select>
-          <option>Voted For</option>
-          <option>Voted Against</option>
+        <select name="bill[<?php echo $bill->getID(); ?>]">
+          <option
+            value="1"
+            <?php if (!empty($votingHistory) && $votingHistory[$bill->getID()] == true): ?>selected<?php endif; ?>
+            >Voted For</option>
+          <option
+            value="0"
+            <?php if (!empty($votingHistory) && $votingHistory[$bill->getID()] == false): ?>selected<?php endif; ?>
+            >Voted Against</option>
         </select>
       </td>
     </tr>
     <?php endforeach; ?>
   </tbody>
 </table>
+<input type="hidden" name="candidate_bill_nonce" value="<?php echo wp_create_nonce('candidate_bill_nonce'); ?>" />
 
 <style type="text/css">
 .candidate_bill_voting_history { width: 100%; }
