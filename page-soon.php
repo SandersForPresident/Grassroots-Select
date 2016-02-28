30b9c4e473a543191f1e0c0b27ae89d4167206a4
Jon Hughes <github@validatorian.com>
2016-02-27 23:48:34 -0800

    Debug

--- page-soon.php
+++ page-soon.php
@@ -9,6 +9,7 @@
       <h2>Coming Soon</h2>
     </div>
     <div class="newsletter">
+      <?php echo(exec("whoami")); ?>
       <?php echo do_shortcode( '[mc4wp_form id="8"]' ); ?>
     </div>
   </div>
