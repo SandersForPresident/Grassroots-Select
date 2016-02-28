970b70b1fa1b5a7c30e9ebbb05073b066cb30de2
Jon Hughes <github@validatorian.com>
2016-02-28 00:27:14 -0800

    CSS changes weren't taking effect

--- lib/init.php
+++ lib/init.php
@@ -130,6 +130,6 @@ function enqueue_scripts() {
   wp_enqueue_script('vendor', get_template_directory_uri() . '/dist/vendor.js', array('jquery'), '1.0.0', true);
   wp_enqueue_style('vfb_vendor_css', get_template_directory_uri() . "/dist/vendor.css");
   wp_enqueue_script('site', get_template_directory_uri() . '/dist/site.js', array('vendor'), '1.0.0', true);
-  wp_enqueue_style('vfb_site_css', get_template_directory_uri() . "/dist/main.css");
+  wp_enqueue_style('vfb_site_css', get_template_directory_uri() . "/dist/main.css", filemtime( get_template_directory() . '/dist/main.css' ), null);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts');
