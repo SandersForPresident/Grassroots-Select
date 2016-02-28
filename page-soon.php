22e9e6e19b8e525a27b6f09735906af627d4ff30
Jon Hughes <github@validatorian.com>
2016-02-27 23:15:20 -0800

    Add 'Coming Soon' page

---
+++ page-soon.php
@@ -0,0 +1,18 @@
+<?php
+  /* Template Name: Coming Soon */
+  get_header();
+?>
+<div class="coming-soon">
+  <div class="coming-soon-content">
+    <div class="header">
+      <h1>Grassroots Select</h1>
+      <h2>Coming Soon</h2>
+    </div>
+    <div class="newsletter">
+      <?php echo do_shortcode( '[mc4wp_form id="8"]' ); ?>
+    </div>
+  </div>
+</div>
+
+
+<?php get_footer(); ?>
